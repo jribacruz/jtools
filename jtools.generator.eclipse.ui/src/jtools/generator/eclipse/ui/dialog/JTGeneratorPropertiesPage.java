6896d1d1b5987190ca1d6fdd2440cbb532aef631
jcruz <jribacruz@gmail.com>
2016-01-15 18:24:56 -0300

    Add do text para o nome do projeto

--- jtools.generator.eclipse.ui/src/jtools/generator/eclipse/ui/dialog/JTGeneratorPropertiesPage.java
+++ jtools.generator.eclipse.ui/src/jtools/generator/eclipse/ui/dialog/JTGeneratorPropertiesPage.java
@@ -2,6 +2,8 @@ package jtools.generator.eclipse.ui.dialog;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -12,7 +14,9 @@ public class JTGeneratorPropertiesPage extends WizardPage {
 
 	private Composite container;
 
-	public static String projectName;
+	private Text textProjectName;
+
+	public static final String JT_GEN_PROJECT_NAME = "jtools.generator.project.name";
 
 	protected JTGeneratorPropertiesPage(String pageName) {
 		super(pageName);
@@ -25,17 +29,46 @@ public class JTGeneratorPropertiesPage extends WizardPage {
 		GridLayout layout = new GridLayout();
 		container.setLayout(layout);
 		layout.numColumns = 2;
-		Label label1 = new Label(container, SWT.NONE);
-		label1.setText("Nome do projeto");
+		
+		Label labelProjectName = new Label(container, SWT.NONE);
+		labelProjectName.setText("Nome do projeto");
 
-		Text textProjectName = new Text(container, SWT.BORDER | SWT.SINGLE);
+		textProjectName = new Text(container, SWT.BORDER | SWT.SINGLE);
 		textProjectName.setText("");
+		textProjectName.addKeyListener(new TextProjectNameKeyListener());
 
 		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
 		textProjectName.setLayoutData(gd);
 
 		setControl(container);
 		setPageComplete(false);
+
+	}
+
+	public String getProjectName() {
+		return textProjectName.getText();
+	}
+
+	/**
+	 * 
+	 * @author jcruz
+	 *
+	 */
+	protected class TextProjectNameKeyListener implements KeyListener {
+
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			if (!((Text) arg0.getSource()).getText().isEmpty()) {
+				setPageComplete(true);
+				return;
+			}
+			setPageComplete(false);
+		}
 
 	}
 

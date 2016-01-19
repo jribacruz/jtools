d6d971fe4c1b1aee91571b620329c8fd3cb45398
jcruz <jribacruz@gmail.com>
2016-01-18 22:26:10 -0300

    jtools.jtwig.core

--- jtools.generator.eclipse.ui/src/jtools/generator/eclipse/ui/handlers/JTProjectEclipseHandler.java
+++ jtools.generator.eclipse.ui/src/jtools/generator/eclipse/ui/handlers/JTProjectEclipseHandler.java
@@ -1,5 +1,10 @@
 package jtools.generator.eclipse.ui.handlers;
 
+import java.io.File;
+
+import jtools.generator.context.Context;
+import jtools.generator.context.MessageContext;
+import jtools.generator.eclipse.ui.Activator;
 import jtools.generator.eclipse.ui.dialog.GeneratorWizard;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -18,9 +23,15 @@ public class JTProjectEclipseHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		/*
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		WizardDialog dialog = new WizardDialog(window.getShell(), new GeneratorWizard());
 		dialog.open();
+			*/
+		File file = Context.getCurrentInstance().load(Activator.PLUGIN_ID, "templates/Activator.vm");
+		
+		MessageContext.add("Msg", file.getAbsolutePath());
+		
 		return null;
 	}
 }

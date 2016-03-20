919d9c2175f663ca8522b77185768fbe9a7324b0
jcruz <jribacruz@gmail.com>
2016-03-19 23:54:59 -0300

    New Functions

---
+++ jtools.commons/src/main/java/jtools/commons/internal/model/demoiselle/function/FunctionMEditViewController.java
@@ -0,0 +1,28 @@
+package jtools.commons.internal.model.demoiselle.function;
+
+import java.io.IOException;
+
+import jtools.commons.internal.model.demoiselle.MEditViewControllerImpl;
+import jtools.commons.model.MClass;
+import jtools.commons.model.demoiselle.MEditViewController;
+
+import com.google.common.base.Function;
+
+/**
+ * 
+ * @author jcruz
+ *
+ */
+public class FunctionMEditViewController implements Function<MClass, MEditViewController> {
+
+	@Override
+	public MEditViewController apply(MClass input) {
+		try {
+			return new MEditViewControllerImpl(input.getFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
---
+++ jtools.commons/src/main/java/jtools/commons/internal/model/demoiselle/function/FunctionMListViewController.java
@@ -0,0 +1,28 @@
+package jtools.commons.internal.model.demoiselle.function;
+
+import java.io.IOException;
+
+import jtools.commons.internal.model.demoiselle.MListViewControllerImpl;
+import jtools.commons.model.MClass;
+import jtools.commons.model.demoiselle.MListViewController;
+
+import com.google.common.base.Function;
+
+/**
+ * 
+ * @author jcruz
+ *
+ */
+public class FunctionMListViewController implements Function<MClass, MListViewController> {
+
+	@Override
+	public MListViewController apply(MClass input) {
+		try {
+			return new MListViewControllerImpl(input.getFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}

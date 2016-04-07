c65584524681cdc4610db6f502c9c0fd7bd3aaa6
jcruz <jribacruz@gmail.com>
2016-04-07 20:28:50 -0300

    Jtools Preferences

---
+++ jtools.eclipse.core/src/jtools/eclipse/core/preferences/JtoolsPreferencePage.java
@@ -0,0 +1,19 @@
+package jtools.eclipse.core.preferences;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class JtoolsPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	@Override
+	public void init(IWorkbench arg0) {
+
+	}
+
+	@Override
+	protected void createFieldEditors() {
+
+	}
+
+}
